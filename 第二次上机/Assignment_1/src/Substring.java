1136bacb531cf6d193ba6fbc05f02ab7b62c0aeb
Hyperion_Z <herbert.zhenghaopeng@outlook.com>
2018-11-14 21:46:08 +0800

    Java experiment Homework2 Assignment1: Add "try-catch" to handle errors.

--- 第二次上机/Assignment_1/src/Substring.java
+++ 第二次上机/Assignment_1/src/Substring.java
@@ -7,9 +7,15 @@
 public class Substring {
 
     public static void main(String[] args) {
-        int beginIndex = Integer.valueOf(args[1]);
-        int endIndex = beginIndex + Integer.valueOf(args[2]);
+        try {
+            int beginIndex = Integer.valueOf(args[1]);
+            int endIndex = beginIndex + Integer.valueOf(args[2]);
+            System.out.println(args[0].substring(beginIndex, endIndex));
+        } catch (Exception e) {
+            System.err.println("Please input correct arguments:");
+            System.err.println("'String' 'Starting index' 'Length of substring'");
+        }
 
-        System.out.println(args[0].substring(beginIndex, endIndex));
+
     }
 }
